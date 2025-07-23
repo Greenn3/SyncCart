71007ee798cd73503be2f1d024b04e3ba2971a4f
Greenn3 <96129501+Greenn3@users.noreply.github.com>
2025-07-23 18:47:25 +0200

    nginx server ssl

--- backend/src/main/java/dev/greenn/backend/item/ItemController.java
+++ backend/src/main/java/dev/greenn/backend/item/ItemController.java
@@ -7,7 +7,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("/api/items")
 public class ItemController {
 private final ItemService itemService;
 
